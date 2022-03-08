c7859071732a346396cc1ebe68b5efffb9ad58e6
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-03-07 23:02:04 -0300

    Requisito 4 feiro.

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -33,6 +34,11 @@ class Header extends Component {
             <div>
               <h1>Header</h1>
               <p data-testid="header-user-name">{ name }</p>
+              <nav>
+                <Link data-testid="link-to-search" to="/search">Search</Link>
+                <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+                <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+              </nav>
             </div>
           )}
       </header>
