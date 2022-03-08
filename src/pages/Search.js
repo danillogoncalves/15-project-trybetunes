bc78305a3a7a5c79d1e2358a022db9c1310c6653
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-03-08 09:21:37 -0300

    Feito as 2 primeiras partes do requisito 6.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: false,
       artistName: '',
+      nameSearched: '',
+      searchResult: [],
       isDisebedButton: true,
     };
   }
@@ -19,37 +24,71 @@ class Search extends Component {
     }));
   }
 
+  handleButton = async (event) => {
+    event.preventDefault();
+    const { value } = event.target;
+    this.setState({
+      loading: true,
+    }, async () => {
+      const searchResult = await searchAlbumsAPI(value);
+      this.setState({
+        loading: false,
+        artistName: '',
+        nameSearched: value,
+        searchResult,
+      });
+    });
+  }
+
   render() {
     const {
+      loading,
       artistName,
+      nameSearched,
+      searchResult,
       isDisebedButton,
     } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          <form>
-            <h1>Search</h1>
-            <div>
-              <input
-                data-testid="search-artist-input"
-                value={ artistName }
-                name="artistName"
-                onChange={ this.handleInput }
-              />
-            </div>
-            <div>
-              <button
-                type="submit"
-                value={ artistName }
-                data-testid="search-artist-button"
-                disabled={ isDisebedButton }
-                onClick={ this.handleButton }
-              >
-                Entrar
-              </button>
-            </div>
-          </form>
+          {loading
+            ? <Loading />
+            : (
+              <form>
+                <h1>Search</h1>
+                <div>
+                  <input
+                    data-testid="search-artist-input"
+                    value={ artistName }
+                    name="artistName"
+                    onChange={ this.handleInput }
+                  />
+                </div>
+                <div>
+                  <button
+                    type="submit"
+                    value={ artistName }
+                    data-testid="search-artist-button"
+                    disabled={ isDisebedButton }
+                    onClick={ this.handleButton }
+                  >
+                    Pesquisar
+                  </button>
+                </div>
+                {searchResult <= 0
+                  ? null
+                  : (
+                    <div>
+                      <p>
+                        Resultado de álbuns de:
+                        {' '}
+                        {nameSearched}
+                      </p>
+                    </div>
+                  )}
+              </form>
+            )}
         </div>
       </>
     );
